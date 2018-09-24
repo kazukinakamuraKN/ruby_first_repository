5cb3b85d5f103ee56ae891fcd36c47bbad4c9398
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-24 09:48:36 +0900

    Add array_test.rb

---
+++ array_test.rb
@@ -0,0 +1,12 @@
+a = [1,20,300]
+#puts a
+#print a
+#p a
+puts "a[0] #=> " "#{a[0]}"
+#use -
+puts "a[-1] #=> " "#{a[-1]}"
+puts a.last
+puts a.first
+puts "a[]の要素数は" + "#{a.length}"
+a_length = a.length
+puts a_length == 3
