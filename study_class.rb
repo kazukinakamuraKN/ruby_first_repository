58317b0c92a11f8f116428629109859bed16458e
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-24 18:31:49 +0900

    class check

---
+++ study_class.rb
@@ -0,0 +1,11 @@
+s = "hello"
+num = 123
+c = 'a'
+flo = 0.123
+dou = 0.0000000000000000000000000000000000000000000000000123
+puts s.class
+puts c.class
+puts num.class
+puts flo.class
+puts dou.class
+#charがでない、doubleがでない
