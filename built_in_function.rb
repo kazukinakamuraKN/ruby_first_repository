3687940c8d3a6cfb9ed685fe966e7caef0ac4d6c
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-25 05:43:53 +0900

    Add built_in_function.rb

---
+++ built_in_function.rb
@@ -0,0 +1,19 @@
+#study 1
+#class String
+#  def palindrome?
+#    self == reverse
+#  end
+#end
+#
+#p "racecar".palindrome?
+#p "onomatopoeia".palindrome?
+#p "Malayalam".downcase.palindrome?
+
+#study 2
+class String
+  def shuffle
+    self.split("").shuffle.join
+  end
+end
+
+p "hello".shuffle
