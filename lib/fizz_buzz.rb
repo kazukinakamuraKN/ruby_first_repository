133cad220defe10cbe0a50496e9c80d6cd3ad955
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-29 19:11:01 +0900

    rgb test OK

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -11,17 +11,9 @@ def fizz_buzz n
   end
 end
 
-require 'minitest/autorun'
 
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal '1', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-    assert_equal 'Buzz', fizz_buzz(5)
-    assert_equal 'FizzBuzz', fizz_buzz(15)
-  end
-end
+
+
 
 #fizz_buzz 1
 #fizz_buzz 3
