f741de235d857c714fc88f721557d2a2007f9287
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-29 20:09:41 +0900

    hex method add

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,9 +1,20 @@
 def to_hex r,g,b
-  hex = '#'
-  [r,g,b].each do |n|
-    hex += n.to_s(16).rjust(2,'0')
+  [r,g,b].inject('#') do |hex,n|
+    hex + n.to_s(16).rjust(2,'0')
   end
-  hex
+#  hex
+# injectは戻り値を返す。
 end
 
 #puts to_hex(13,14,15)
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+    [r,g,b].each do |s|
+      ints << s.hex
+    end
+  ints
+end
